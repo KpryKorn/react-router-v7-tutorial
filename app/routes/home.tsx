5c475a7b26affcf97f7f801e1c7edace00e465de
KpryKorn <roffinisacha@gmail.com>
2024-12-17 11:12:12 +0100

    trying out new react router v7 features

---
+++ app/routes/home.tsx
@@ -0,0 +1,10 @@
+export default function Home() {
+  return (
+    <p id="index-page">
+      This is a demo for React Router v7.
+      <br />
+      Check out <a href="https://reactrouter.com">the website</a> for more
+      information.
+    </p>
+  );
+}
